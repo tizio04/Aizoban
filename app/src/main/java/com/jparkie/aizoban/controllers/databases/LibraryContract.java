89e233f1634a5f62a8712729b68cca72bfd01473
CheviOne <marceldzi@gmail.com>
2015-11-09 19:06:23 +0100

    Updated Mangafox thumbnail urls

--- app/src/main/java/com/jparkie/aizoban/controllers/databases/LibraryContract.java
+++ app/src/main/java/com/jparkie/aizoban/controllers/databases/LibraryContract.java
@@ -1,7 +1,7 @@
 package com.jparkie.aizoban.controllers.databases;
 
 public class LibraryContract {
-    public static final int DATABASE_VERSION = 7;
+    public static final int DATABASE_VERSION = 8;
     public static final String DATABASE_NAME = "AizobanLibrary.db";
 
     private LibraryContract() {
